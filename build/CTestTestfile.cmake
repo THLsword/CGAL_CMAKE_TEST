5eb8d4439cad52142b39ce44dc225b13fcea8c6d
THLsword <604300468@qq.com>
2021-02-05 15:26:44 +0800

    修改文件夾

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/CGAL-study/CGAL_CMAKE_TEST
-# Build directory: D:/CGAL-study/CGAL_CMAKE_TEST/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
